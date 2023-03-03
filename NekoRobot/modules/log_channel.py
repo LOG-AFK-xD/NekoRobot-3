952e66e221973abdc9d39b94246df32b4022cc98
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2023-03-03 22:13:06 +0530

    Update log_channel.py

--- NekoRobot/modules/log_channel.py
+++ NekoRobot/modules/log_channel.py
@@ -8,13 +8,12 @@ FILENAME = __name__.rsplit(".", 1)[-1]
 if is_module_loaded(FILENAME):
     from telegram import ParseMode, Update
     from telegram.error import BadRequest, Unauthorized
-    from telegram.ext import CommandHandler, JobQueue, run_async
+    from telegram.ext import CommandHandler, JobQueue
     from telegram.utils.helpers import escape_markdown
 
-    from NekoRobot import EVENT_LOGS, LOGGER, dispatcher
+    from NekoRobot import EVENT_LOGS, LOGGER, NEKO_PTB
     from NekoRobot.modules.helper_funcs.chat_status import user_admin
     from NekoRobot.modules.sql import log_channel_sql as sql
-
     def loggable(func):
         @wraps(func)
         def log_action(
